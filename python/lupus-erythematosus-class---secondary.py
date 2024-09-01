a810dad371324d6b33fae6be1999d41883154467
Martin Chapman <contact@martinchapman.co.uk>
2024-09-01 13:16:38 +0100

    Update made by Phenoflow

---
+++ python/lupus-erythematosus-class---secondary.py
@@ -0,0 +1,25 @@
+# phekb, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"4002526","system":"ICD10CM"},{"code":"4178133","system":"ICD10CM"},{"code":"4186940","system":"ICD10CM"},{"code":"4250483","system":"ICD10CM"},{"code":"4267801","system":"ICD10CM"},{"code":"4297164","system":"ICD10CM"},{"code":"4186940","system":"ICD10CM"},{"code":"4002526","system":"ICD10CM"},{"code":"4267801","system":"ICD10CM"},{"code":"4178133","system":"ICD10CM"},{"code":"4250483","system":"ICD10CM"},{"code":"4297164","system":"ICD10CM"},{"code":"4002526","system":"ICD10CM"},{"code":"4178133","system":"ICD10CM"},{"code":"4186940","system":"ICD10CM"},{"code":"4250483","system":"ICD10CM"},{"code":"4267801","system":"ICD10CM"},{"code":"4297164","system":"ICD10CM"},{"code":"4186940","system":"ICD10CM"},{"code":"4002526","system":"ICD10CM"},{"code":"4267801","system":"ICD10CM"},{"code":"4178133","system":"ICD10CM"},{"code":"4250483","system":"ICD10CM"},{"code":"4297164","system":"ICD10CM"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('lupus-erythematosus-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["lupus-erythematosus-class---secondary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["lupus-erythematosus-class---secondary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["lupus-erythematosus-class---secondary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
